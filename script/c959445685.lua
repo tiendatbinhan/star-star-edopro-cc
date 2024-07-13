8e5a2b4244072395c56e5c0418a1c6b6570b22aa
Tien Dat <tiendatbinhan@gmail.com>
2024-07-13 18:56:44 +0700

    Add helper functions for easier code/maintainance

--- script/c959445685.lua
+++ script/c959445685.lua
@@ -1,6 +1,7 @@
 --☆Star-Star☆ StarPet Imp
 local s,id = GetID()
 Duel.LoadScript("custom_const.lua")
+Duel.LoadScript("helper_function.lua")
 
 function s.initial_effect(c)
     Pendulum.AddProcedure(c)
@@ -12,8 +13,8 @@ function s.initial_effect(c)
     e1:SetType(EFFECT_TYPE_IGNITION)
     e1:SetRange(LOCATION_PZONE)
     e1:SetCountLimit(1, {id, 0})
-    e1:SetTarget(s.addtg)
-    e1:SetOperation(s.addop)
+    e1:SetTarget(StarStar.StarPetSearchTarget(ATTRIBUTE_FIRE))
+    e1:SetOperation(StarStar.StarPetSearchOperation(ATTRIBUTE_FIRE))
     c:RegisterEffect(e1)
 
     local e2 = Effect.CreateEffect(c)
@@ -27,34 +28,6 @@ function s.initial_effect(c)
     e3:SetCode(EFFECT_IMMUNE_EFFECT)
     e3:SetValue(s.immuneval)
     c:RegisterEffect(e3)
-end
-
---Add from GY effect
-function s.addfilter(c)
-    return c:IsAbleToHand() and c:IsSetCard(SET_STARSTAR) and c:IsAttribute(ATTRIBUTE_FIRE)
-end
-
-function s.addtg(e, tp, eg, ep, ev, re, r, rp, chk)
-    local c = e:GetHandler()
-    if chk == 0
-    then
-        return Duel.IsExistingMatchingCard(s.addfilter, tp, LOCATION_GRAVE, 0, 1, nil) and c:IsDestructable(e) and not Duel.IsPlayerAffectedByEffect(tp, CARD_NECROVALLEY)
-    end
-    Duel.SetOperationInfo(0, CATEGORY_TOHAND, nil, 1, tp, LOCATION_GRAVE)
-    Duel.SetOperationInfo(0, CATEGORY_DESTROY, c, 0, tp, 0)
-end
-
-function s.addop(e, tp, eg, ep, ev, re, r, rp)
-    local c = e:GetHandler()
-    if Duel.Destroy(c, REASON_EFFECT) > 0
-        and Duel.IsExistingMatchingCard(s.addfilter, tp, LOCATION_GRAVE, 0, 1, nil)
-        and not Duel.IsPlayerAffectedByEffect(tp, CARD_NECROVALLEY)
-    then
-        Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_ATOHAND)
-        local tc = Duel.SelectMatchingCard(tp, s.addfilter, tp, LOCATION_GRAVE, 0, 1, 1, nil)
-        Duel.SendtoHand(tc, tp, REASON_EFFECT)
-        Duel.ConfirmCards(1-tp, tc)
-    end
 end
 
 --Attack up
--- script/helper_function.lua
+++ script/helper_function.lua
@@ -2,6 +2,7 @@ if not StarStar then
     StarStar = {}
 end
 
+--For flip effect of non-Pendulum Monster
 function StarStar.FlipEffectFilter(c, attribute)
     return c:IsAbleToHand() and c:IsSetCard(SET_STARSTAR) and c:IsType(TYPE_PENDULUM) and c:IsType(TYPE_MONSTER) and c:IsAttribute(attribute)
 end
@@ -26,6 +27,7 @@ function StarStar.CreateFlipEffectOperation(attribute)
     end
 end
 
+--For SP Summon of most non-StarPet Pendulum Monsters
 function StarStar.AddNonUnionPendulumProcedure(c)
     local e = Effect.CreateEffect(c)
     e:SetCategory(CATEGORY_SPECIAL_SUMMON + CATEGORY_TODECK)
@@ -60,5 +62,37 @@ function StarStar.NonUnionProcOperation(e, tp, eg, ep, ev, re, r, rp, chk)
     local gc = Duel.SelectMatchingCard(tp, StarStar.NonUnionReturnFilter, tp, LOCATION_MZONE, 0, 1, 1, nil)
     if Duel.SendtoDeck(gc, tp, SEQ_DECKSHUFFLE, REASON_EFFECT) > 0 then
         Duel.SpecialSummon(c, SUMMON_TYPE_SPECIAL, tp, tp, false, false, POS_FACEUP)
+    end
+end
+
+--For StarPet search effect in Pendulum Zone
+function StarStar.StarPetSearchFilter(c, att)
+    return c:IsAbleToHand() and c:IsSetCard(SET_STARSTAR) and c:IsAttribute(att)
+end
+
+function StarStar.StarPetSearchTarget(attribute)
+    return function(e, tp, eg, ep, ev, re, r, rp, chk)
+        local c = e:GetHandler()
+        if chk == 0
+        then
+            return Duel.IsExistingMatchingCard(StarStar.StarPetSearchFilter, tp, LOCATION_GRAVE, 0, 1, nil, attribute) and c:IsDestructable(e) and not Duel.IsPlayerAffectedByEffect(tp, CARD_NECROVALLEY)
+        end
+        Duel.SetOperationInfo(0, CATEGORY_TOHAND, nil, 1, tp, LOCATION_GRAVE)
+        Duel.SetOperationInfo(0, CATEGORY_DESTROY, c, 0, tp, 0)
+    end
+end
+
+function StarStar.StarPetSearchOperation(attribute)
+    return function(e, tp, eg, ep, ev, re, r, rp)
+        local c = e:GetHandler()
+        if Duel.Destroy(c, REASON_EFFECT) > 0
+            and Duel.IsExistingMatchingCard(StarStar.StarPetSearchFilter, tp, LOCATION_GRAVE, 0, 1, nil, attribute)
+            and not Duel.IsPlayerAffectedByEffect(tp, CARD_NECROVALLEY)
+        then
+            Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_ATOHAND)
+            local tc = Duel.SelectMatchingCard(tp, StarStar.StarPetSearchFilter, tp, LOCATION_GRAVE, 0, 1, 1, nil, attribute)
+            Duel.SendtoHand(tc, tp, REASON_EFFECT)
+            Duel.ConfirmCards(1-tp, tc)
+        end
     end
 end
