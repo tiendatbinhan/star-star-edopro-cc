4f7fb863f4a80abb5b49ff9d5d6ff51bc8d7b7dd
Tien Dat <31890037+tiendatbinhan@users.noreply.github.com>
2024-09-19 09:32:08 +0700

    Fix bug

--- script/c951616589.lua
+++ script/c951616589.lua
@@ -36,9 +36,8 @@ function s.target(e, tp, eg, ep, ev, re, r, rp, chk, chkc)
 
     if chk == 0
     then
-        return Duel.IsExistingTarget(s.filter, tp, LOCATION_MZONE, 0, 1, nil)
-        and Duel.IsExistingTarget(s.filter, tp, LOCATION_PZONE, 0, 1, nil)
-        and Duel.GetLocationCount(tp, LOCATION_MZONE) > 0
+        local available_zone, _ = Duel.GetLocationCount(tp, LOCATION_MZONE)
+        return Duel.IsExistingTarget(s.filter, tp, LOCATION_MZONE, 0, 1, nil) and Duel.IsExistingTarget(s.filter, tp, LOCATION_PZONE, 0, 1, nil) and available_zone > 0
     end
 
     Duel.Hint(HINT_SELECTMSG, tp, HINTMSG_SPSUMMON)
